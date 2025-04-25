01d7f7200dbe0aca7a1c4b1d4510b9eb306b3709
Todd Tanner <todd@spawndev.com>
2025-04-25 09:25:54 -0400

    Add project files.

---
+++ SimpleWebWorkersExample/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SimpleWebWorkersExample;
+using SpawnDev.BlazorJS;
+using SpawnDev.BlazorJS.WebWorkers;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddBlazorJSRuntime();
+builder.Services.AddWebWorkerService();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().BlazorJSRunAsync();
